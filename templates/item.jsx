d4748655721f7c7281cf3d9e567b21b918dc2701
Andy Mardesich <andy.mardesich@ucop.edu>
2016-08-01 17:42:19 -0700

    ES6 conversion of item page

--- templates/item.jsx
+++ templates/item.jsx
@@ -55,144 +55,127 @@ class ItemPage extends React.Component
 {/* Tabbed Navigation courtesy Trey Piepmeier http://codepen.io/trey/post/tabbed-navigation-react*/}
 var tabList = [
   { 'id': 1, 'name': 'Main content', 'url': '/main' },
-  { 'id': 2, 'name': 'Data & media    ', 'url': '/suppl' },
-  { 'id': 3, 'name': 'Metrics    ', 'url': '/metrics' },
-  { 'id': 4, 'name': 'Author & article info    ', 'url': '/authorarticle' },
+  { 'id': 2, 'name': 'Data & media', 'url': '/suppl' },
+  { 'id': 3, 'name': 'Metrics', 'url': '/metrics' },
+  { 'id': 4, 'name': 'Author & article info', 'url': '/authorarticle' },
   { 'id': 5, 'name': 'Comments', 'url': '/comments' }
 ];
 
-var Tab = React.createClass({
-  handleClick: function(e){
+class Tab extends React.Component {
+  handleClick(e){
     e.preventDefault();
     this.props.handleClick();
-  },
-  
-  render: function(){
-    return (
-      <li className="nav-item">
-        <a className={this.props.isCurrent ? 'current' : null} 
-           onClick={this.handleClick}
-           href={this.props.url}>{this.props.name}&nbsp;&nbsp;
-        </a>
-      </li>
-    );
   }
-});
+  
+  render() { return(
+    <li className="nav-item">
+      <a className={this.props.isCurrent ? 'current' : null} 
+         onClick={this.handleClick}
+         href={this.props.url}>{this.props.name}&nbsp;&nbsp;
+      </a>
+    </li>
+  )}
+}
 
-var Tabs = React.createClass({
-  handleClick: function(tab){
+class Tabs extends React.Component {
+  handleClick(tab){
     this.props.changeTab(tab);
-  },
+  }
   
-  render: function(){
-    return (
-      <ul className="nav nav-tabs">
-      {this.props.tabList.map(function(tab) {
-        return (
-          <Tab
-            handleClick={this.handleClick.bind(this, tab)}
-            key={tab.id}
-            url={tab.url}
-            name={tab.name}
-            isCurrent={(this.props.currentTab === tab.id)}
-           />
-        );
-      }.bind(this))}
-      </ul>
-    );
-  }
-});
+  render() { return(
+    <ul className="nav nav-tabs">
+    {this.props.tabList.map(function(tab) {
+      return (
+        <Tab
+          handleClick={this.handleClick.bind(this, tab)}
+          key={tab.id}
+          url={tab.url}
+          name={tab.name}
+          isCurrent={(this.props.currentTab === tab.id)}
+         />
+      );
+    }.bind(this))}
+    </ul>
+  )}
+}
 
 
-var Content = React.createClass({
-  render: function(){
-    return(
-      <div>
-        {this.props.currentTab === 1 ? <ContentMain/> : null }
-        {this.props.currentTab === 2 ? <ContentSuppl/> : null}
-        {this.props.currentTab === 3 ? <ContentMetrics/> : null}
-        {this.props.currentTab === 4 ? <ContentAuthArt/> : null}
-        {this.props.currentTab === 5 ? <ContentComments/> : null}
-      </div>
-    );
-  }
-});
+class Content extends React.Component {
+  render() { return(
+    <div>
+      {this.props.currentTab === 1 ? <ContentMain/> : null }
+      {this.props.currentTab === 2 ? <ContentSuppl/> : null}
+      {this.props.currentTab === 3 ? <ContentMetrics/> : null}
+      {this.props.currentTab === 4 ? <ContentAuthArt/> : null}
+      {this.props.currentTab === 5 ? <ContentComments/> : null}
+    </div>
+  )}
+}
 
-var ContentMain = React.createClass({
-  render: function(){
-    return(
-      <div className="content">
-        Item main content here 
-      </div>
-    );
-  }
-});
+class ContentMain extends React.Component {
+  render() { return(
+    <div className="content">
+      Item main content here 
+    </div>
+  )}
+}
 
-var ContentSuppl = React.createClass({
-  render: function(){
-    return(
-      <div className="content">
-        Data &amp; Media content here
-      </div>
-    );
-  }
-});
+class ContentSuppl extends React.Component {
+  render() { return(
+    <div className="content">
+      Data &amp; Media content here
+    </div>
+  )}
+}
 
-var ContentMetrics = React.createClass({
-  render: function(){
-    return(
-      <div className="content">
-        Metrics content here
-      </div>
-    );
-  }
-});
+class ContentMetrics extends React.Component {
+  render() { return(
+    <div className="content">
+      Metrics content here
+    </div>
+  )}
+}
 
-var ContentAuthArt = React.createClass({
-  render: function(){
-    return(
-      <div className="content">
-        Author &amp; Article content here
-      </div>
-    );
-  }
-});
+class ContentAuthArt extends React.Component {
+  render() { return(
+    <div className="content">
+      Author &amp; Article content here
+    </div>
+  )}
+}
 
-var ContentComments = React.createClass({
-  render: function(){
-    return(
-      <div className="content">
-        Comments content here
-      </div>
-    );
-  }
-});
+class ContentComments extends React.Component {
+  render() { return(
+    <div className="content">
+      Comments content here
+    </div>
+  )}
+}
 
-var ItemTabbed = React.createClass({
-  getInitialState: function () {        
-    return {
+class ItemTabbed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       tabList: tabList,
       currentTab: 1
     };
-  },
-
-  changeTab: function(tab) {
-    this.setState({ currentTab: tab.id });
-  },
-
-  render: function(){
-    return(
-      <div>
-        <Tabs
-          currentTab={this.state.currentTab}
-          tabList={this.state.tabList}
-          changeTab={this.changeTab}
-        />
-        <Content currentTab={this.state.currentTab} />
-      </div>
-    );
   }
-});
+
+  changeTab(tab) {
+    this.setState({ currentTab: tab.id });
+  }
+
+  render() { return(
+    <div>
+      <Tabs
+        currentTab={this.state.currentTab}
+        tabList={this.state.tabList}
+        changeTab={this.changeTab}
+      />
+      <Content currentTab={this.state.currentTab} />
+    </div>
+  )}
+}
 
 // Render everything under the single top-level div created in the base HTML. As its
 // initial properties, pass it the chunk of initialData included in the base HTML.
